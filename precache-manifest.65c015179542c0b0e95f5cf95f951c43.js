7f280681e71095c16ff1f3ecd981c36fe755383e
KrishayNair <krishay958@gmail.com>
2023-08-29 12:45:19 +0530

    Updates

---
+++ precache-manifest.65c015179542c0b0e95f5cf95f951c43.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e3ab99743a2e2cf42938d952d03735",
+    "url": "/EcellWebsite/index.html"
+  },
+  {
+    "revision": "9008683b4dd932cb5c23",
+    "url": "/EcellWebsite/static/css/2.32048716.chunk.css"
+  },
+  {
+    "revision": "efa6c0576c2e345f7b5d",
+    "url": "/EcellWebsite/static/css/main.d633efb9.chunk.css"
+  },
+  {
+    "revision": "9008683b4dd932cb5c23",
+    "url": "/EcellWebsite/static/js/2.e3d3758a.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/EcellWebsite/static/js/2.e3d3758a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa6c0576c2e345f7b5d",
+    "url": "/EcellWebsite/static/js/main.71a51b7c.chunk.js"
+  },
+  {
+    "revision": "7619d7f10116c2c86132",
+    "url": "/EcellWebsite/static/js/runtime-main.97b1deab.js"
+  },
+  {
+    "revision": "f85903d473cb19505e2aa6c63a0db00b",
+    "url": "/EcellWebsite/static/media/athlete-small.f85903d4.png"
+  },
+  {
+    "revision": "6d53898d42640dad92347101b160da7f",
+    "url": "/EcellWebsite/static/media/athlete2.6d53898d.png"
+  },
+  {
+    "revision": "fa03b2f8f7b99367702f0e0c8c5460ba",
+    "url": "/EcellWebsite/static/media/clock.fa03b2f8.svg"
+  },
+  {
+    "revision": "b43c6ec52e265645250f6b037cb913a0",
+    "url": "/EcellWebsite/static/media/event1.1.b43c6ec5.jpeg"
+  },
+  {
+    "revision": "231170e38475e4b016322f0edab50fc2",
+    "url": "/EcellWebsite/static/media/event1.2.231170e3.jpeg"
+  },
+  {
+    "revision": "b07665c50f78176d4d2e22254ba9c474",
+    "url": "/EcellWebsite/static/media/event1.3.b07665c5.jpg"
+  },
+  {
+    "revision": "f7188eff36d8373da3c180d821fc607e",
+    "url": "/EcellWebsite/static/media/event1.f7188eff.jpeg"
+  },
+  {
+    "revision": "dd3410958b7a8d06bbf139107ef8439b",
+    "url": "/EcellWebsite/static/media/event2.1.dd341095.jpeg"
+  },
+  {
+    "revision": "bba2ab122b3b5ec0153b249166b1641b",
+    "url": "/EcellWebsite/static/media/event2.2.bba2ab12.jpeg"
+  },
+  {
+    "revision": "9128ac35adda1d4edf044c94862467eb",
+    "url": "/EcellWebsite/static/media/event2.3.9128ac35.jpeg"
+  },
+  {
+    "revision": "4c49346876be01cdc3f4ba07eede2382",
+    "url": "/EcellWebsite/static/media/event2.4c493468.jpeg"
+  },
+  {
+    "revision": "fd78f9a69c61a49fd366d1aa8c9dccfe",
+    "url": "/EcellWebsite/static/media/event3.1.fd78f9a6.jpeg"
+  },
+  {
+    "revision": "54d16a54d530f7a9fb39bea28a1bc6d7",
+    "url": "/EcellWebsite/static/media/event3.2.54d16a54.jpeg"
+  },
+  {
+    "revision": "bbf5399ee77e6d61707d2dbfc0c020c0",
+    "url": "/EcellWebsite/static/media/event3.bbf5399e.jpeg"
+  },
+  {
+    "revision": "6e3e50ab94d856eba00d0808a83ef923",
+    "url": "/EcellWebsite/static/media/event3_3.6e3e50ab.jpeg"
+  },
+  {
+    "revision": "e149f59c6323b18f98e987cd564e1fc9",
+    "url": "/EcellWebsite/static/media/good-times2.e149f59c.jpg"
+  },
+  {
+    "revision": "169a9b3ae8eff8e916dc7711c7546c64",
+    "url": "/EcellWebsite/static/media/goodtimes-small.169a9b3a.png"
+  },
+  {
+    "revision": "3900feb4f1372bb1391037d8774e74c7",
+    "url": "/EcellWebsite/static/media/heroImg3.3900feb4.jpg"
+  },
+  {
+    "revision": "caa59aa622ef68d3e2d19b27c723148e",
+    "url": "/EcellWebsite/static/media/homeNew.caa59aa6.svg"
+  },
+  {
+    "revision": "3a28ace8c1c16b63fa45d9e1d732f565",
+    "url": "/EcellWebsite/static/media/image10.3a28ace8.jpg"
+  },
+  {
+    "revision": "754a0c4ca755e6bb834f59fc6731f7b5",
+    "url": "/EcellWebsite/static/media/image11.754a0c4c.jpg"
+  },
+  {
+    "revision": "5fc375618e1769c1e11392806e7956eb",
+    "url": "/EcellWebsite/static/media/image12.5fc37561.jpg"
+  },
+  {
+    "revision": "8e35c981b9c5362094c8f4e4448c82ca",
+    "url": "/EcellWebsite/static/media/image13.8e35c981.jpg"
+  },
+  {
+    "revision": "68ec04cf80fccacfb8524d2451d308c0",
+    "url": "/EcellWebsite/static/media/image14.68ec04cf.jpg"
+  },
+  {
+    "revision": "42be56f124f879139a983e005de3131a",
+    "url": "/EcellWebsite/static/media/image15.42be56f1.jpg"
+  },
+  {
+    "revision": "9802b1477926058ead320d786a8e0c2d",
+    "url": "/EcellWebsite/static/media/image2.9802b147.jpg"
+  },
+  {
+    "revision": "9106d0e5a8f227f148946a3a5b53294c",
+    "url": "/EcellWebsite/static/media/image3.9106d0e5.jpg"
+  },
+  {
+    "revision": "a76d7336af38bcc3da52c9466fa13cde",
+    "url": "/EcellWebsite/static/media/image4.a76d7336.jpg"
+  },
+  {
+    "revision": "3e3576deef5c000af58664b281894b1c",
+    "url": "/EcellWebsite/static/media/image5.3e3576de.jpg"
+  },
+  {
+    "revision": "f843907309bc61fff144d18c9d9580f9",
+    "url": "/EcellWebsite/static/media/image6.f8439073.jpg"
+  },
+  {
+    "revision": "8c2c6c8af00bc7164a48e199f7360410",
+    "url": "/EcellWebsite/static/media/image7.8c2c6c8a.jpg"
+  },
+  {
+    "revision": "090889c6d2e9f99c625f45fb86e9a8a0",
+    "url": "/EcellWebsite/static/media/image8.090889c6.jpg"
+  },
+  {
+    "revision": "5d5ea10738e6123ef2d2d80568791e88",
+    "url": "/EcellWebsite/static/media/image9.5d5ea107.jpg"
+  },
+  {
+    "revision": "3cbba7b2381408971f3f65ae80928980",
+    "url": "/EcellWebsite/static/media/logoNew.3cbba7b2.svg"
+  },
+  {
+    "revision": "d98711a9a6265436c4db55078fa0e3a4",
+    "url": "/EcellWebsite/static/media/team1.d98711a9.jpg"
+  },
+  {
+    "revision": "4d4ad81686b7e5d24ef457eb7187a689",
+    "url": "/EcellWebsite/static/media/teamwork.4d4ad816.svg"
+  },
+  {
+    "revision": "74873026dbed7229712447d165a7a2fd",
+    "url": "/EcellWebsite/static/media/the-racer2.74873026.jpg"
+  },
+  {
+    "revision": "ca518f44c6e1ddc450654002971bb467",
+    "url": "/EcellWebsite/static/media/theracer-small.ca518f44.png"
+  }
+]);
